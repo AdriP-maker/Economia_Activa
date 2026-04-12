4a396ae3c82022d324685fe30ef51e3dcb700b40
JosueFlores19 <josueedsau.flores@utp.ac.pa>
2026-04-12 13:43:35 -0500

    a ver si no explota ptm Adrian

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "pa.ac.utp.economia_activa"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/pa/ac/utp/economia_activa/Home.kt
+++ app/src/main/java/pa/ac/utp/economia_activa/Home.kt
@@ -2,10 +2,22 @@ package pa.ac.utp.economia_activa
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Looper
+import android.os.Handler
+
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_Economia_Activa) //vulve al tema principal
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(/* layoutResID = */ R.layout.activity_home)
+
+        // Delay de 2 segundos y luego ir a MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // para que no regrese al splash
+        }, 1000)
     }
 }
